301a1f8b545854736b831c1312f1a62226ed4b44
Guido Eduardo <guidoeduardonsilva@gmail.com>
2023-03-04 12:01:38 -0300

    feat: astro add TailwindCSS and sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,12 @@ import { defineConfig } from 'astro/config';
 import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
+import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: vercel()
+  adapter: vercel(),
+  integrations: [sitemap(), tailwind()]
 });
