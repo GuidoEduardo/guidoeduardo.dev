04110d2e4564912d419d552cd411f8e5781608e9
Guido Eduardo <guidoeduardonsilva@gmail.com>
2023-10-29 21:55:05 -0300

    feat: recreating website

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,19 +5,17 @@ module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     fontFamily: {
-      sans: ['Poppins', ...defaultTheme.fontFamily.sans],
-      sixcaps: ['"Six Caps"', ...defaultTheme.fontFamily.sans],
-      pontano: ['"Pontano Sans"', ...defaultTheme.fontFamily.sans],
-      spartan: ['"League Spartan"', ...defaultTheme.fontFamily.sans],
+      sans: ['Jost', ...defaultTheme.fontFamily.sans],
+      serif: ['"Marcellus SC"', ...defaultTheme.fontFamily.serif],
+      handle: ['"Handle Change"', ...defaultTheme.fontFamily.sans],
+      mono: ['"Major Mono Display"', ...defaultTheme.fontFamily.sans],
+      syncopate: ['Syncopate', ...defaultTheme.fontFamily.sans]
     },
     extend: {
       colors: {
-        primary: "#F9FBFD",
-        secondary: "#0C0C0C",
-        third: "#E02323",
-      },
-      backgroundImage: {
-        'post-texture': "url('/hover.webp')",
+        black: "#030303",
+        white: "#DFE7EE",
+        grey: "#212325"
       }
     },
   },
